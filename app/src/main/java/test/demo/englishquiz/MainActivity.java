7533cc2bdcf002d15216825bae98b01a328b84a9
YYC <aideeplearningdemo@gmail.com>
2018-09-11 20:44:17 +0800

    2018/9/11

---
+++ app/src/main/java/test/demo/englishquiz/MainActivity.java
@@ -0,0 +1,135 @@
+package test.demo.englishquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mScoreView;
+    private TextView mQuestion;
+
+    private Button mButtonChoice1,mButtonChoice2,mButtonChoice3,mButtonChoice4;
+
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
+    private String mAnswer;
+
+    private Firebase mQusetionRef,mChoice1Ref,mChoice2Ref,mChoice3Ref,mChoice4Ref,mAnswerRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestion = (TextView)findViewById(R.id.question);
+
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+        mButtonChoice4 = (Button)findViewById(R.id.choice4);
+
+        updateQuestion();
+    }
+
+    public void updateQuestion(){
+        mQusetionRef =  new Firebase("https://quiztesting-dce48.firebaseio.com/"+ mQuestionNumber +"/question");
+
+        mQusetionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String question = dataSnapshot.getValue(String.class);
+                mQuestion.setText(question);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mChoice1Ref = new Firebase("https://quiztesting-dce48.firebaseio.com/"+ mQuestionNumber +"/choice1");
+
+        mChoice1Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice1.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mChoice2Ref = new Firebase("https://quiztesting-dce48.firebaseio.com/"+ mQuestionNumber +"/choice2");
+
+        mChoice2Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice2.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mChoice3Ref = new Firebase("https://quiztesting-dce48.firebaseio.com/"+ mQuestionNumber +"/choice3");
+
+        mChoice3Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice3.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mChoice4Ref = new Firebase("https://quiztesting-dce48.firebaseio.com/"+ mQuestionNumber +"/choice4");
+
+        mChoice4Ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String choice = dataSnapshot.getValue(String.class);
+                mButtonChoice4.setText(choice);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mAnswerRef = new Firebase("https://quiztesting-dce48.firebaseio.com/"+ mQuestionNumber +"/Answer");
+        mAnswerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mAnswer = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mQuestionNumber++;
+    }
+}
